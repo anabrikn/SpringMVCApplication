46af2ac03a025bfc09bcc43f49c9d041f1f617d4
Anna <anabrikn@gmail.com>
2021-02-08 23:58:07 +0300

    @Valid

--- src/main/java/ru/ava/springcoursecrudapp/controllers/PeopleController.java
+++ src/main/java/ru/ava/springcoursecrudapp/controllers/PeopleController.java
@@ -1,11 +1,16 @@
 package ru.ava.springcoursecrudapp.controllers;
 
+//import jakarta.validation.Valid;
+//import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import ru.ava.springcoursecrudapp.dao.PersonDAO;
 import ru.ava.springcoursecrudapp.models.Person;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/people")
@@ -46,7 +51,11 @@ public class PeopleController {
     }
 
     @PostMapping()
-    public String create(@ModelAttribute("person") Person person) {
+    public String create(@ModelAttribute("person") @Valid Person person,
+                         BindingResult bindingResult) {
+        if(bindingResult.hasErrors()) {
+            return "people/new";
+        }
         personDAO.save(person);
         return "redirect:/people";
     }
@@ -58,7 +67,12 @@ public class PeopleController {
     }
 
     @PatchMapping("/{id}")
-    public String update(@ModelAttribute("person") Person person, @PathVariable("id") int id) {
+    public String update(@ModelAttribute("person") @Valid Person person,
+                         BindingResult bindingResult,
+                         @PathVariable("id") int id) {
+        if (bindingResult.hasErrors()) {
+            return "people/edit";
+        }
         personDAO.update(id, person);
         return "redirect:/people";
     }
--- src/main/java/ru/ava/springcoursecrudapp/models/Person.java
+++ src/main/java/ru/ava/springcoursecrudapp/models/Person.java
@@ -1,9 +1,22 @@
 package ru.ava.springcoursecrudapp.models;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 public class Person {
     private int id;
+
+    @NotEmpty(message = "Name must not be empty")
+    @Size(min = 2, max = 30, message = "Name must be between 2 and 30 characters")
     private String name;
+
+    @Min(value = 0, message = "Age cannot be less than 0")
     private int age;
+
+    @NotEmpty(message = "Email must not be empty")
+    @Email(message = "Email must be valid")
     private String email;
 
     public Person() {
